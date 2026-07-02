abe175bb9e437fbf844c9bd9a449ef5c577edd0f
SO-SO <sys107502@gmail.com>
2026-07-02 16:46:20 +0900

    feat: Footer 구현

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="border border-neutral-700  w-full bg-gray-100">
+                <p className="text-sm font-medium t">
+                    © 2026 유레카 리엑트 젠지 팬 사이트
+                    <br />
+                    제작자: 소윤서 @이메일: sys107502@gmail.com
+                    <br />
+                    Special Thanks To : Chovy & 해준-Park
+                </p>
+            </div>
+        </footer>
+    );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/layout/Header";
+import { Footer } from "../../components/layout/Footer";
 
 export const Home = () => {
     return (
         <main>
             <Header />
+            <br />
+            <Footer />
         </main>
     );
 };
