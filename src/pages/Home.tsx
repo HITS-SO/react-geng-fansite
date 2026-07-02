288e6f5c93005275786e62ae8e145fa538a9f63c
SO-SO <sys107502@gmail.com>
2026-07-02 16:55:32 +0900

    refactor: 프로젝트 폴더 구조 개선

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Header } from "../../components/layout/Header";
-import { Footer } from "../../components/layout/Footer";
+import { Header } from "../components/layout/Header";
+import { Footer } from "../components/layout/Footer";
 
 export const Home = () => {
     return (
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../pages/Home/Home";
+import { Home } from "../pages/Home";
 
 const router = createBrowserRouter([{ path: "", element: <Home /> }]);
 export default router;
