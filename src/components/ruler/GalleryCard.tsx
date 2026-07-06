a12d6006a4ddd1e213eb5147733094cc7b9392ff
SO-SO <sys107502@gmail.com>
2026-07-06 14:31:58 +0900

    fix: 룰러 페이지의 갤러리 카드 컴포넌트에서 사진 클릭시 모달 창으로 해당하는 사진을 띄우도록 수정

--- src/components/ruler/GalleryCard.tsx
+++ src/components/ruler/GalleryCard.tsx
@@ -1,62 +1,82 @@
+import { useState } from "react";
+
 interface GalleryCardProps {
     image: string;
     alt: string;
 }
 
 const GalleryCard = ({ image, alt }: GalleryCardProps) => {
-    return (
-        <div
-            className="
-                group
-                relative
-                overflow-hidden
-                rounded-2xl
-                shadow-lg
-            "
-        >
-            <img
-                src={image}
-                alt={alt}
-                className="
-                    h-72
-                    w-full
-                    object-cover
-                    transition-all
-                    duration-500
-                    group-hover:scale-110
-                    group-hover:brightness-50
-                "
-            />
+    const [isOpen, setIsOpen] = useState(false);
 
+    return (
+        <>
+            {/* 카드 */}
             <div
                 className="
-                    absolute
-                    inset-0
-                    flex
-                    items-center
-                    justify-center
-                    opacity-0
-                    transition-opacity
-                    duration-300
-                    group-hover:opacity-100
+                    group
+                    relative
+                    overflow-hidden
+                    rounded-2xl
+                    shadow-lg
+                    cursor-pointer
                 "
+                onClick={() => setIsOpen(true)}
             >
-                <span
+                <img
+                    src={image}
+                    alt={alt}
                     className="
-                        rounded-full
-                        border
-                        border-white
-                        px-6
-                        py-2
-                        text-lg
-                        font-semibold
-                        text-white
+                        h-72
+                        w-full
+                        object-cover
+                        transition-all
+                        duration-500
+                        group-hover:scale-110
+                        group-hover:brightness-50
+                    "
+                />
+
+                <div
+                    className="
+                        absolute
+                        inset-0
+                        flex
+                        items-center
+                        justify-center
+                        opacity-0
+                        transition-opacity
+                        duration-300
+                        group-hover:opacity-100
                     "
                 >
-                    View
-                </span>
+                    <span className="rounded-full border border-white px-6 py-2 text-lg font-semibold text-white">
+                        View
+                    </span>
+                </div>
             </div>
-        </div>
+
+            {/* 모달 */}
+            {isOpen && (
+                <div
+                    className="
+                        fixed
+                        inset-0
+                        z-50
+                        flex
+                        items-center
+                        justify-center
+                        bg-black/80
+                    "
+                    onClick={() => setIsOpen(false)}
+                >
+                    <img
+                        src={image}
+                        alt={alt}
+                        className="max-h-[90vh] max-w-[90vw] rounded-xl shadow-2xl"
+                    />
+                </div>
+            )}
+        </>
     );
 };
 
